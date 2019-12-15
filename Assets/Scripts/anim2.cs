fe0189d06e19a23495b9febcf6cc95b428d36a6b
Mohammad Nafis Naufally <naf1s.n4ufally@gmail.com>
2019-12-15 20:00:08 +0700

    Init

---
+++ Assets/Scripts/anim2.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using UnityEngineUI;
+// using Vuforia;
+
+public class anim2 : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void animIdle()
+    {
+        bool play = anim.GetBool("play");
+        anim.SetBool("play", true);
+    }
+
+    public void animRun()
+    {
+        bool playRun = anim.GetBool("play_run");
+        anim.SetBool("play_run", true);
+    }
+
+    public void animJump()
+    {
+        bool playJump = anim.GetBool("play_jump");
+        anim.SetBool("play_jump", true);
+    }
+
+    public void animWalk()
+    {
+        bool playWalk = anim.GetBool("play_walk");
+        anim.SetBool("play_walk", true);
+    }
+
+    public void animSprint()
+    {
+        bool playSprint = anim.GetBool("play_sprint");
+        anim.SetBool("play_sprint", true);
+    }
+
+    public void animBackwards()
+    {
+        bool playBackwards = anim.GetBool("play_backwards");
+        anim.SetBool("play_backwards", true);
+    }
+
+    public void animStop()
+    {
+        bool play = anim.GetBool("play");
+        bool playRun = anim.GetBool("play_run");
+        bool playJump = anim.GetBool("play_jump");
+        bool playWalk = anim.GetBool("play_walk");
+        bool playSprint = anim.GetBool("play_sprint");
+        bool playBackwards = anim.GetBool("play_backwards");
+
+        anim.SetBool("play", false);
+        anim.SetBool("play_run", false);
+        anim.SetBool("play_jump", false);
+        anim.SetBool("play_walk", false);
+        anim.SetBool("play_sprint", false);
+        anim.SetBool("play_backwards", false);
+    }
+}
---
+++ Assets/Scripts/vb_anim.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vb_anim : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject anim_btn;
+    // public GameObject anim_btn2;
+    public Animator anim1;
+    // public Animator anim2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim_btn = GameObject.Find("anim_btn"); 
+        anim_btn.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        anim1.GetComponent<Animator>();
+
+        // anim_btn2 = GameObject.Find("anim_btn2"); 
+        // anim_btn2.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        // anim2.GetComponent<Animator>();
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        if (anim1 != null)
+            {
+                // anim1.Play("Idle01");
+                bool play = anim1.GetBool("play");
+
+                anim1.SetBool("play", true);
+            }
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        if (anim1 != null)
+            {
+                // anim1.Play("none");
+                bool play = anim1.GetBool("play");
+
+                anim1.SetBool("play", false);
+            }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
